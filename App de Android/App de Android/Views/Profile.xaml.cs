d4a0abb27faf4a2d8a2ac21ff599d8ec786b97ce
miguelguerra1907187 <mike123guerra@hotmail.com>
2024-11-04 09:48:09 -0600

    Added Profile frames with exit

--- App de Android/App de Android/Views/Profile.xaml.cs
+++ App de Android/App de Android/Views/Profile.xaml.cs
@@ -10,30 +10,47 @@ namespace App_de_Android.Views
             InitializeComponent();
         }
 
-        // Métodos para manejar los eventos de clic de los botones
-        private void Boton1_Clicked(object sender, EventArgs e)
+        private void Frame1_Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Botón 1", "Has presionado el botón 1: Hola, Mundo", "OK");
+            // Lógica para el marco 1
+            DisplayAlert("Tapped", "Marco 1 fue tocado", "OK");
         }
 
-        private void Boton2_Clicked(object sender, EventArgs e)
+        private void Frame2_Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Botón 2", "Has presionado el botón 2: Bienvenido", "OK");
+            // Lógica para el marco 2
+            DisplayAlert("Tapped", "Marco 2 fue tocado", "OK");
         }
 
-        private void Boton3_Clicked(object sender, EventArgs e)
+        private void Frame3_Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Botón 3", "Has presionado el botón 3: Presiona aquí", "OK");
+            // Lógica para el marco 3
+            DisplayAlert("Tapped", "Marco 3 fue tocado", "OK");
         }
 
-        private void Boton4_Clicked(object sender, EventArgs e)
+        private void Frame4_Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Botón 4", "Has presionado el botón 4: Xamarin es genial", "OK");
+            // Lógica para el marco 4
+            DisplayAlert("Tapped", "Marco 4 fue tocado", "OK");
         }
 
-        private void Boton5_Clicked(object sender, EventArgs e)
+        private void Frame5_Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Botón 5", "Has presionado el botón 5: ¡Prueba completada!", "OK");
+            // Lógica para el marco 5
+            DisplayAlert("Tapped", "Marco 5 fue tocado", "OK");
         }
+
+        private async void Frame6_Tapped(object sender, EventArgs e)
+        {
+            // Lógica para el marco 6
+            await Navigation.PushAsync(new LoginScreen());
+        }
+
+
+
+
+
+
+
     }
 }
