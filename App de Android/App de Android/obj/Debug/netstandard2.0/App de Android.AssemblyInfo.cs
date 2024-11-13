cda0562750fa674e098aacf4e3e5ea020d91c784
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2024-11-12 18:21:06 -0600

    Se mejora diseño de pantalla de Login y Register

--- App de Android/App de Android/obj/Debug/netstandard2.0/App de Android.AssemblyInfo.cs
+++ App de Android/App de Android/obj/Debug/netstandard2.0/App de Android.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App de Android")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c42be94bc402398f9a9ee4531b0e699e7359b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed46830d168444af11f4cd323660e9bbe1223b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("App de Android")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App de Android")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
